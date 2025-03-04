064c26a01f3cdbd228d916dd013a998691d843c5
1Lazare <127741385+1Lazare@users.noreply.github.com>
2025-03-05 00:55:12 +0400

    chore: create user module configurate typescript and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,11 +17,11 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 2020, // Ensure the version is set to support `import.meta`
       sourceType: 'module',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname, // Set the tsconfigRootDir explicitly
       },
     },
   },
@@ -29,7 +29,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
